22a57b34bc087cf1de89d3d9edab44df69d5cb77
Kelly Johnson <kelly@leanplum.com>
2017-11-08 14:14:09 -0800

    Initial leanplum integration

--- app/src/main/java/com/astronautwannabe/nasa/myapplication/MainActivity.java
+++ app/src/main/java/com/astronautwannabe/nasa/myapplication/MainActivity.java
@@ -3,18 +3,32 @@ package com.astronautwannabe.nasa.myapplication;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+
 import java.util.Random;
+
 import android.widget.TextView;
 import android.widget.Button;
 import android.widget.ImageView;
 
+import com.leanplum.Leanplum;
+import com.leanplum.LeanplumPushService;
+import com.leanplum.annotations.Parser;
+import com.leanplum.annotations.Variable;
+import com.leanplum.callbacks.VariablesChangedCallback;
+
 public class MainActivity extends AppCompatActivity {
 
+    // All variables must be defined before calling Leanplum.start.
+    // Use the Parser class (see docs) since your variables are outside of your main activity.
+    @Variable
+    public static String welcomeMessage = "Welcome to Leanplum!";
+
     String answers[] = {"I don't think so", "Ask again later",
-            "No.", "Leave Pusheen alone", "Pusheen doesn't like you so no"
+            "No.", "Leave BLAh alone", "Pusheen doesn't like you so no"
             , "Yes because I <3 you", "Only if you buy me food", "Yuuhhh", "Mhm.",
             "Let pizza decide"};
 
@@ -45,16 +59,45 @@ public class MainActivity extends AppCompatActivity {
                 // 6
             }
         });
+
+        setupLeanplum();
     }
+
+    private void setupLeanplum() {
+        String server = "kelly-dot-java-api-dot-leanplum-staging.appspot.com";
+        String appID = "app_N59g6QL2dVfbcEiih1tgrcItbrjipxdheGZ3AHBci2w";
+        String dbgKey = "dev_BnjHjeJa4M3MIC0hVwKHr4Wh3AfhimhHDBCNgBZfOPw";
+        Leanplum.setAppIdForDevelopmentMode(appID, dbgKey);
+
+
+        LeanplumPushService.setGcmSenderId(LeanplumPushService.LEANPLUM_SENDER_ID);
+
+        // Be sure to set the context to this in the Parser.
+        Parser.parseVariables(this);
+
+        // It's important to use the variables changed callback if the value is needed
+        // around the time the app starts, so that we're guaranteed to have the latest value.
+        Leanplum.addVariablesChangedHandler(new VariablesChangedCallback() {
+            @Override
+            public void variablesChanged() {
+                Log.i("Test", welcomeMessage);
+            }
+        });
+
+        Leanplum.setApiConnectionSettings(server, "api", true);
+        Leanplum.start(this, "Meowz");
+        Leanplum.setUserId("Meowz");
+    }
+
     @Override
-    public boolean onCreateOptionsMenu (Menu menu){
+    public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
     @Override
-    public boolean onOptionsItemSelected (MenuItem item){
+    public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
